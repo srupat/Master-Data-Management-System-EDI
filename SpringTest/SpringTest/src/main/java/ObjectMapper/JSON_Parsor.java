bb8a681d9ebc598854570227dd9a41fabe6075d3
AbheeravPatankar <akp0304@gmail.com>
2024-02-02 12:06:27 +0530

    parse JSON using jackson

---
+++ SpringTest/SpringTest/src/main/java/ObjectMapper/JSON_Parsor.java
@@ -0,0 +1,28 @@
+package ObjectMapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JSON_Parsor {
+	
+	private static ObjectMapper objectMapper = getDefaultObjectMapper();
+	
+	private static ObjectMapper getDefaultObjectMapper() {
+		
+		ObjectMapper defaultObjectMapper = new ObjectMapper();
+		defaultObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		return defaultObjectMapper;
+	}
+	
+	public static JsonNode parse(String json) throws JsonMappingException, JsonProcessingException {
+		return objectMapper.readTree(json);	
+	}
+	
+	public static <A> A fromJson(JsonNode jNode, Class<A> classArg) throws JsonProcessingException, IllegalArgumentException {
+		return objectMapper.treeToValue(jNode, classArg);
+	}
+				
+}
--- SpringTest/SpringTest/src/main/java/com/example/Sample/SpringTest/collection/Attribute.java
+++ SpringTest/SpringTest/src/main/java/com/example/Sample/SpringTest/collection/Attribute.java
@@ -4,28 +4,19 @@ class Attribute {
 	private String attribute_name;
 	private String attribute_type;
 	
-	public Attribute() {	};
-	
-	public Attribute(String attribute_name, String attribute_type) {
-		
-		this.attribute_name = attribute_name;
-		this.attribute_type = attribute_type;	
-	}
-	
 	public String getAttribute_name() {
-	    return attribute_name;
+		return attribute_name;
 	}
-
 	public void setAttribute_name(String attribute_name) {
-	    this.attribute_name = attribute_name;
+		this.attribute_name = attribute_name;
 	}
-
 	public String getAttribute_type() {
-	    return attribute_type;
+		return attribute_type;
 	}
-
 	public void setAttribute_type(String attribute_type) {
-	    this.attribute_type = attribute_type;
+		this.attribute_type = attribute_type;
 	}
+	
+	
 	
 }
