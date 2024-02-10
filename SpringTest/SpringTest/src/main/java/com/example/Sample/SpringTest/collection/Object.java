04a098c90b169758b9c65d03c543126d85323d1a
srupat <srujanpatwardhan@gmail.com>
2024-02-10 12:42:49 +0530

    object controllers and json parsing

--- SpringTest/SpringTest/src/main/java/com/example/Sample/SpringTest/collection/Object.java
+++ SpringTest/SpringTest/src/main/java/com/example/Sample/SpringTest/collection/Object.java
@@ -2,7 +2,10 @@ package com.example.Sample.SpringTest.collection;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.lang.Float;
 import java.util.List;
+
+import java.lang.*;
 
 
 @Document(collection = "object")
@@ -11,14 +14,58 @@ public class Object {
     String obj_name;
     List<Attribute_Object> attributes;
 
-    public Object() {
+    public Object(String obj_template, String obj_name, List<Attribute_Object> attributes) {
+        this.obj_template = obj_template;
+        this.obj_name = obj_name;
+        this.attributes = attributes;
     }
 
     public void addNewObj(Attribute_Object obj){
         attributes.add(obj);
     }
 
-    public void createNewObj(String )
+    public void createNewObj(String name, String val, String type){
+        // check from the template the datatype of this object having the same name
+        switch(type){
+            case "int":
+                try {
+                    Attribute_Object obj = new Int(name, Integer.parseInt(val));
+                    addNewObj(obj);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "float":
+                try {
+                    Attribute_Object obj;
+                    obj = new com.example.Sample.SpringTest.collection.Float(name, Float.parseFloat(val));
+                    addNewObj(obj);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "bool":
+                try {
+                    Attribute_Object obj;
+                    obj = new com.example.Sample.SpringTest.collection.Bool(name, Boolean.parseBoolean(val));
+                    addNewObj(obj);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            default:
+                try {
+                    Attribute_Object obj;
+                    obj = new com.example.Sample.SpringTest.collection.Strings(name, val);
+                    addNewObj(obj);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            // for parsing of obj type, first form an object using jackson from the given json
+        }
+
+    }
 
 
 
--- SpringTest/SpringTest/src/main/java/com/example/Sample/SpringTest/controller/ObjController.java
+++ SpringTest/SpringTest/src/main/java/com/example/Sample/SpringTest/controller/ObjController.java
@@ -1,14 +1,21 @@
 package com.example.Sample.SpringTest.controller;
 
+import com.example.Sample.SpringTest.collection.Attribute_Object;
 import com.example.Sample.SpringTest.collection.Object;
 import com.example.Sample.SpringTest.repository.ObjectRepository;
+import com.example.Sample.SpringTest.service.TemplateService;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.List;
+
+import static ObjectMapper.JSON_Parsor.objectMapper;
 
 @RestController
 public class ObjController {
@@ -16,8 +23,43 @@ public class ObjController {
     @Autowired
     ObjectRepository orepo;
 
+    @Autowired
+    private TemplateService templateService;
+
     @PostMapping("/create/object")
-    public Object submitObject(@RequestBody Object object) { return orepo.save(object); }
+    public Object submitObject(@RequestBody String jsonString) {
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        try{
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+
+            String objName = jsonNode.get("obj_name").asText();
+            String objTemplate = jsonNode.get("obj_template").asText();
+            String attributeType = templateService.getAttributeType(objTemplate);
+            System.out.println(objName + objTemplate + attributeType);
+
+
+            List<Attribute_Object> attributes = objectMapper.convertValue(jsonNode.get("attributes"), new TypeReference<List<Attribute_Object>>() {});
+            Object obj = new Object(objTemplate, objName, attributes);
+            // get the name and val in string from the json string attributes
+            for (JsonNode attributeNode : jsonNode.get("attributes")) {
+                String attributeName = attributeNode.get("name").asText();
+                String attributeValue = attributeNode.get("val").asText();
+                System.out.println(attributeName + attributeValue);
+
+                obj.createNewObj(attributeName, attributeValue, attributeType);
+            }
+
+            orepo.save(obj);
+
+
+
+            }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     @GetMapping("/objects")
     public List<Object> getAllObjects(){
