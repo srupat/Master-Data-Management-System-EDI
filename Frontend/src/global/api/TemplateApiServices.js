b96b0acbf9ee64b490e2316488d4fa3dfbea4a5d
Aaditya Patil <131251920+Aaditatgithub@users.noreply.github.com>
2024-03-26 11:44:14 +0530

    payroll check basisc

--- Frontend/src/global/api/TemplateApiServices.js
+++ Frontend/src/global/api/TemplateApiServices.js
@@ -1,12 +1,12 @@
-    import { apiClient } from "./ApiClient"
+import { apiClient } from "./ApiClient"
 
-    export const createTemplateApi = 
-        (templateJson) =>  apiClient.post(`/create/template`, templateJson)
+export const createTemplateApi = 
+    (templateJson) =>  apiClient.post(`/create/template`, templateJson)
 
-    export const getAllTemplateForExpressionCreation = () => apiClient.get('/templates')
+export const getAllTemplates = () => apiClient.get('/templates')
 
-    export const submitExpressionForAttributeAttachment = 
-        (templateName,AttributeName,expression) => apiClient.put(`/template/attachAttributeExpression/${templateName}/${AttributeName}/${expression}`) 
+export const submitExpressionForAttributeAttachment = 
+    (templateName,AttributeName,expression) => apiClient.put(`/template/attachAttributeExpression/${templateName}/${AttributeName}/${expression}`) 
 
-        export const submitExpressionForTemplateAttachment = 
-        (templateName, expJson) => apiClient.put(`/template/attachTemplateExpression/${templateName}`, expJson)
+export const submitExpressionForTemplateAttachment = 
+    (templateName, expJson) => apiClient.put(`/template/attachTemplateExpression/${templateName}`, expJson)
